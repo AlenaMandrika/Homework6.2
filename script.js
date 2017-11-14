65cf0e77485aa24edf3b1bc15f8c55373f348d24
alenamandrika <Alenenok0@ukr.net>
2017-11-14 21:51:03 +0200

    closed function

---
+++ script.js
@@ -0,0 +1,47 @@
+function User(nameUser, ageUser, sexUser) {
+    this.nameUser = nameUser || 'nameUser';
+
+    var name = nameUser;
+    var age = +ageUser || 0;
+
+    (function () {
+        if (sexUser === 'men') {
+            name = 'mr' + nameUser
+        }
+        if (sexUser === 'women') {
+            name = 'ms' + name
+        }
+
+    })();
+    
+    function isAdult (age) {
+        return age > 17
+    }
+
+    this.getNameUser = function () {
+        return name || 'Bublik'
+    };
+
+    this.setName = function (newName) {
+        if (typeof newName === 'string' && newName.length > 3) {
+            name = newName;
+            return name;
+        }
+        else {
+            return new Error('too short')
+        }
+    };
+
+    this.getSecretFiles = function () {
+      if(isAdult(age)){
+          return 'some strage files'
+      } else {
+         return new Error('you are too young')
+      }
+    };
+}
+
+var Alena = new User('Alena', '33', 'women');
+
+
+
